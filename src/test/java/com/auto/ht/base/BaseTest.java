3d621d840deef31b186b80f83d2ad176e9d8b299
Hieu Tran <hieucan1991@gmail.com>
2025-07-09 16:17:15 +0700

    Update vj test cases

--- src/test/java/com/auto/ht/base/BaseTest.java
+++ src/test/java/com/auto/ht/base/BaseTest.java
@@ -1,7 +1,7 @@
 package com.auto.ht.base;
 
 import com.auto.ht.utils.Constants;
-import com.auto.ht.utils.LazyPropertiesHelper;
+import com.auto.ht.helpers.LazyPropertiesHelper;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
